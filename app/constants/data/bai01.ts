234880f3b3d288d398c83a7f2e476dd02ff5c10d
Le Khai Hoan <khaihoan2306@gmail.com>
2023-10-14 20:46:36 +0700

    add bai01 content

---
+++ app/constants/data/bai01.ts
@@ -0,0 +1,181 @@
+export const bai01 = {
+  grammar: [
+    {
+      example: [
+        { korean: "화입니다.", vietnamese: "Tôi là Hoa." },
+        { korean: "학생입니다.", vietnamese: "Tôi là học sinh." },
+        { korean: "베트남 사람입니다.", vietnamese: "Tôi là người Việt Nam." },
+        { korean: "뚜안입니다.", vietnamese: "Tôi là Tuấn." },
+        { korean: "회사원입니다.", vietnamese: "Tôi là nhân viên công ty." },
+      ],
+      vTitle: "là...",
+      content:
+        "이다, gắn vào danh từ, có nghĩa tương đương trong tiếng Việt là 'là'. Hình thức kính ngữ của '이다' là '입니다', thường dùng trong câu trần thuật.",
+      kTitle: "입니다",
+    },
+    {
+      example: [
+        { korean: "남 씨입니까?", vietnamese: "Anh là Nam phải không?" },
+        { korean: "학생입니까?", vietnamese: "Anh là học sinh phải không?" },
+        { korean: "베트남 사람입니까?", vietnamese: "Anh là người Việt Nam phải không?" },
+        { korean: "디엠 씨입나까?", vietnamese: "Chị là Diễm phải không?" },
+        { korean: "회사원입나까?", vietnamese: "Anh/Chị là nhân viên công ty phải không?" },
+      ],
+      vTitle: "(là)...phải không?",
+      content:
+        '입니까, là hình thức nghi vấn (hỏi) của 입니다, có nghĩa tương đương tiếng Việt "là...phải không?"',
+      kTitle: "입니까",
+    },
+    {
+      vTitle: "",
+      example: [
+        { korean: "저는 남입나다.", vietnamese: "Tôi là Nam." },
+        { korean: "제 이름은 남입니다.", vietnamese: "Tên tôi là Nam." },
+        { korean: "남 씨는 학생입니까?", vietnamese: "Nam là học sinh phải không?" },
+        { korean: "조민재 씨는 회사원입니다.", vietnamese: "Jo Min Jae là nhân viên công ty." },
+        { korean: "선생님은 한국 사람입니다.", vietnamese: "Giáo viên là người Hàn Quốc." },
+      ],
+      content:
+        "Gắn vào sau danh từ để chỉ ra danh từ đó chính là chủ thể của hành động. 은 kết hợp với những danh từ có phụ âm cuối. 는 kết hợp với những danh từ không có phụ âm cuối. Có trường hợp 'danh từ + 은/는' được giản lược đi khi chủ thể đó đã được đề cập trước đó.",
+      kTitle: "은/는",
+    },
+  ],
+  vocabulary: [
+    {
+      vTitle: "Quốc gia",
+      content: [
+        {
+          korean: "한국",
+          vietnamese: "Hàn Quốc",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fkorea.jpeg?alt=media&token=a5d226dc-29dd-4373-8c23-438376d8026e",
+        },
+        {
+          korean: "베트남",
+          vietnamese: "Việt Nam",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fvietnam.png?alt=media&token=6ab1ec54-bb8e-4a10-96a3-504259370ec6",
+        },
+        {
+          korean: "말레이시아",
+          vietnamese: "Malaysia",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fmalaysia.jpeg?alt=media&token=e2d3a1d6-6625-4289-b8cd-59f8431fa997",
+        },
+        {
+          korean: "일본",
+          vietnamese: "Nhật Bản",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fjapan.png?alt=media&token=16f18f8a-67e0-4107-a8bc-1a6bb7a84074",
+        },
+        {
+          korean: "미국",
+          vietnamese: "Mỹ",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fusa.png?alt=media&token=f0353c64-b0c9-4529-9b55-d4cf2a6babf8",
+        },
+        {
+          korean: "중국",
+          vietnamese: "Trung Quốc",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fchina.jpeg?alt=media&token=3cf02aa0-f5d3-4a77-a541-e0660774b981",
+        },
+        {
+          korean: "태국",
+          vietnamese: "Thái Lan",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fthailand.jpeg?alt=media&token=f4391c6a-f7ce-4590-9ace-0dc3b1973da0",
+        },
+        {
+          korean: "호주",
+          vietnamese: "Úc",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Faustralia.jpeg?alt=media&token=a6e7b4d1-70fb-4601-a72a-f4f3bbb9ce06",
+        },
+        {
+          korean: "몽골",
+          vietnamese: "Mông Cổ",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fmongolian.jpeg?alt=media&token=28cc1c52-1591-4049-be78-0e1257097636",
+        },
+        {
+          korean: "인도네시아",
+          vietnamese: "Indonesia",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Findonesia.png?alt=media&token=9c7458fd-7a95-456c-b669-7d3e0a322c50",
+        },
+        {
+          korean: "필리핀",
+          vietnamese: "Phillipin",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fphillipin.webp?alt=media&token=0e82c7a5-e8f6-47f2-a1a5-b54a6fc1b6fe",
+        },
+        {
+          korean: "인도",
+          vietnamese: "Ấn Độ",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Findia.webp?alt=media&token=73a87ca2-2912-4e35-abdc-067bf373b330",
+        },
+        {
+          korean: "영국",
+          vietnamese: "Anh",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fengland.webp?alt=media&token=8e670bcc-418b-43a9-997d-6d97d8b1a6ba",
+        },
+        {
+          korean: "독일",
+          vietnamese: "Đức",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Fgermany.webp?alt=media&token=3e900b9a-fafe-438c-a5cf-778e0fdc1396",
+        },
+        {
+          korean: "프랑스",
+          vietnamese: "Pháp",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Ffrance.jpeg?alt=media&token=7e46f991-d037-407a-b0e4-807ad0bae162",
+        },
+        {
+          korean: "러시아",
+          vietnamese: "Nga",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/korean-b640f.appspot.com/o/bai-01%2Frussia.png?alt=media&token=d25b62c6-a16a-4bc3-aebe-4ba258367d80",
+        },
+      ],
+      kTitle: "나라",
+    },
+    {
+      vTitle: "Nghề nghiệp",
+      content: [
+        { korean: "학생", vietnamese: "học sinh" },
+        { korean: "회사원", vietnamese: "nhân viên công ty" },
+        { korean: "은행원", vietnamese: "nhân viên ngân hàng" },
+        { korean: "선생님", vietnamese: "giáo viên" },
+        { korean: "의사", vietnamese: "bác sĩ" },
+        { korean: "공무원", vietnamese: "công chức" },
+        { korean: "관광 가이드", vietnamese: "hướng dẫn viên du lịch" },
+        { korean: "주부", vietnamese: "nội trợ" },
+        { korean: "약사", vietnamese: "dược sĩ" },
+        { korean: "운전기사", vietnamese: "lái xe" },
+      ],
+      kTitle: "직업",
+    },
+    {
+      vTitle: "Lời chào",
+      content: [
+        { korean: "안녕하세요", vietnamese: "Xin chào" },
+        { korean: "안녕하십니까", vietnamese: "Xin chào" },
+        { korean: "안녕히 가세요", vietnamese: "Xin tạm biệt. (Đi về bình an)" },
+        { korean: "안녕히 계세요", vietnamese: "Xin tạm biệt. (Ở lại bình an)" },
+        { korean: "처음 뵙겠습니다", vietnamese: "Rất hân hạnh" },
+        { korean: "반갑습니다", vietnamese: "Rất vui được gặp" },
+      ],
+      kTitle: "인사말",
+    },
+  ],
+  practice: [
+    { korean: "저는 학생입니다", vietnamese: "Tôi là học sinh." },
+    { korean: "저는 미국 사람입니다", vietnamese: "Tôi là người Mỹ." },
+    { korean: "남 씨는 회사원입니다", vietnamese: "Anh Nam là nhân viên văn phòng." },
+    { korean: "이유나 씨는 한국 사람입니다", vietnamese: "Lee Yoo Na là người Hàn Quốc." },
+  ],
+}
